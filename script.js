74fd49d77b8e387dec36cc1b50cfa5c42539255e
goliathgrouper <vitalijsimon48@gmail.com>
2024-03-25 17:42:41 +0300

    Make function operate()

--- script.js
+++ script.js
@@ -13,3 +13,20 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+function operate(num1, oper, num2) {
+    switch (oper) {
+        case '+':
+            return add(num1, num2);
+        case '-':
+            return subtract(num1, num2);
+        case '*':
+            return multiply(num1, num2);
+        case '/':
+            return divide(num1, num2);
+    }
+}
+
+let num1;
+let num2;
+let oper;
